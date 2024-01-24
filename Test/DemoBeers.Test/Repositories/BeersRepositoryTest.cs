1e8162e8a2f930dea87f6cf65075d0941d2e75fb
mperaza18 <miguel.peraza18@outlook.com>
2024-01-23 19:18:05 -0600

    Add Unit Test

---
+++ Test/DemoBeers.Test/Repositories/BeersRepositoryTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoBeers.Test.Repositories
+{
+    public class BeersRepositoryTest
+    {
+        [Fact]
+        public async Task GivenThereAreBeers_WhenPassingId_ThenShouldReturnASingleBeer()
+        {
+
+        }
+    }
+}
